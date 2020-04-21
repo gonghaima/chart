b7e7c49b990e0aef5de952970e6305e0cc934d0c
Steven <gonghaima@hotmail.com>
2020-04-21 11:52:57 +1000

    svg group element

--- src/BasicShape.js
+++ src/BasicShape.js
@@ -6,6 +6,12 @@ export default () => {
             <svg width="960" height="500">
                 <circle cx="50" cy="50" r="40"></circle>
                 <rect x="100" y="25" width="50" height="50"></rect>
+                <circle cx="50" cy="150" r="40" fill="red"></circle>
+                <rect x="100" y="125" width="50" height="50" fill="green"></rect>
+                <g transform="translate(0,200)">
+                    <circle cx="50" cy="50" r="40"></circle>
+                    <rect x="100" y="25" width="50" height="50"></rect>
+                </g>
             </svg>
         </div>
     )
