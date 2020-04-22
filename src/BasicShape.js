d732b880b7fa54f818e98d56ef4e17f56d7596c6
Steven <gonghaima@hotmail.com>
2020-04-22 15:04:10 +1000

    translate - alignment

--- src/BasicShape.js
+++ src/BasicShape.js
@@ -13,7 +13,7 @@ export default () => {
                         <circle cx="50" cy="50" r="40" stroke-width="5"></circle>
                         <rect x="100" y="25" width="50" height="50"></rect>
                     </g>
-                    <g className="lines">
+                    <g className="lines" transform="translate(50,0)">
                         <line x1="200" y1="20" x2="300" y2="280"></line>
                         <path fill="none" d="M300 280 L350 200 L400 250 L450 230"></path>
                     </g>
