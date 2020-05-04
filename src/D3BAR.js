08e1cc7d72066c51caaadc30ea1b87b87da115e9
Steven <gonghaima@hotmail.com>
2020-05-04 18:22:26 +1000

    rectangle respresent data

--- src/D3BAR.js
+++ src/D3BAR.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react'
 
 import * as d3 from 'd3'
-import { arc, csv } from 'd3'
+import { csv } from 'd3'
 import data from './data/d3/worldPopulation.csv'
 
 // import *  as dd from './data/d3/worldPopulation.csv'
@@ -9,16 +9,31 @@ import data from './data/d3/worldPopulation.csv'
 
 
 export const D3BAR = () => {
+    const visEl = useRef(null);
+    const width = '400';
+    const height = '400';
+
     useEffect(() => {
+
+        const svg = d3
+            .select(visEl.current)
+            .append('svg');
+        svg.attr('width', width);
+        svg.attr('height', height);
+
         csv(data).then(data => {
             data.forEach(d => d.population = +d.population * 1000);
             console.log(data);
+            svg.selectAll().data(data).enter().append('rect')
+                .attr('width', 300)
+                .attr('height', 300)
         }).catch(err => {
         })
     });
     return (
         <div>
             hello
+            <div ref={visEl}></div>
         </div>
     );
 }
