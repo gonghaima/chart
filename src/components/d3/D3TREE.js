950cf3c6bdeffd6228f778e06fa479e807300b6b
Steven <gonghaima@hotmail.com>
2020-08-06 09:35:17 +1000

    add data

--- src/components/d3/D3TREE.js
+++ src/components/d3/D3TREE.js
@@ -3,12 +3,16 @@ import React, { useRef, useEffect } from 'react'
 import { select, arc, event, json, tsv, geoPath, geoMercator, geoOrthographic, geoEquirectangular } from 'd3';
 import { feature } from "topojson";
 
+import data from "../../data/d3/countryHierarchy.json"
+
 const basicSvgStyle = {
     height: '100vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
 };
+
+//https://bl.ocks.org/curran/1dd7ab046a4ed32380b21e81a38447aa
 
 export const D3TREE = () => {
     const visEl = useRef(null);
@@ -29,7 +33,7 @@ export const D3TREE = () => {
             .attr('height', height)
             .attr('rx', 40);
 
-
+        console.log(data);
     });
     return (
         <div style={basicSvgStyle} ref={visEl}>
