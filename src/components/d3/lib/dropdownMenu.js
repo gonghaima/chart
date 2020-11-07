1b7940c5e18de310fe26f062a0ada3d5fe35b176
Steven <gonghaima@hotmail.com>
2020-11-08 10:00:52 +1100

    log value on select option change

--- src/components/d3/lib/dropdownMenu.js
+++ src/components/d3/lib/dropdownMenu.js
@@ -7,11 +7,14 @@
 
 
 export const dropdownMenu = (selection, props) => {
-    const { options } = props;
+    const { options, onOptionClicked } = props;
 
     let select = selection.selectAll('select').data([null]);
     select = select.enter().append('select')
         .merge(select)
+        .on('change', function () {
+            console.log(this.value);
+        })
         .attr('class', 'select-menu');
 
     const option = select.selectAll('option').data(options);
